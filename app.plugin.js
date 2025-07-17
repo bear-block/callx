1af3d9c203ac4bf4acd6b30316e0edb4572aa0dd
bear-block <hao.dev7@gmail.com>
2025-07-17 23:51:00 +0700

    fix: export plugin directly in app.plugin.js for Expo compatibility

--- app.plugin.js
+++ app.plugin.js
@@ -1,12 +1,1 @@
-const withCallx = require('./plugin/build');
-
-/**
- * A config plugin for configuring Callx - React Native incoming call UI library
- *
- * @param {Object} config - Expo config
- * @param {Object} props - Plugin props
- * @param {string} [props.mode='native'] - Mode: 'native' (adds FCM service) or 'js' (no service)
- */
-module.exports = (config, props = {}) => {
-  return withCallx(config, props);
-};
+module.exports = require('./plugin/build');
