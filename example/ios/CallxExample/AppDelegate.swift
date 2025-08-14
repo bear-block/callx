1c93dcf85c11402b438dcfc83fb87387f93c22f9
hao.le <thanhh.ext@medgate.ch>
2025-08-14 20:52:19 +0700

    chore(example-ios): revert AppDelegate to default RN template; keep FirebaseApp.configure()

--- example/ios/CallxExample/AppDelegate.swift
+++ example/ios/CallxExample/AppDelegate.swift
@@ -2,32 +2,27 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import CallKit
-import PushKit
 import FirebaseCore
 
 @main
-class AppDelegate: RCTAppDelegate {
-  var reactNativeDelegate: ReactNativeDelegate!
-  // Factory will be set in didFinishLaunchingWithOptions
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
 
-  override func application(
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+  func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    // Configure Firebase - follow React Native Firebase docs
     FirebaseApp.configure()
-    
-    // Initialize Callx native PushKit handler early
-    // Note: This will be handled by the native module initialization
-    // We don't need to manually call it here to avoid potential crashes
-    
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
 
     reactNativeDelegate = delegate
-    self.reactNativeFactory = factory
+    reactNativeFactory = factory
 
     window = UIWindow(frame: UIScreen.main.bounds)
 
@@ -38,33 +33,6 @@ class AppDelegate: RCTAppDelegate {
     )
 
     return true
-  }
-
-  // MARK: - Remote Notifications
-
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    print("📱 Device token received: \(deviceToken.map { String(format: "%02.2hhx", $0) }.joined())")
-  }
-
-  override func application(
-    _ application: UIApplication,
-    didFailToRegisterForRemoteNotificationsWithError error: Error
-  ) {
-    print("❌ Failed to register for remote notifications: \(error.localizedDescription)")
-  }
-
-  // MARK: - Background App Refresh
-
-  override func application(
-    _ application: UIApplication,
-    performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-  ) {
-    // Handle background app refresh for VoIP
-    print("🔄 Background app refresh triggered")
-    completionHandler(.newData)
   }
 }
 
